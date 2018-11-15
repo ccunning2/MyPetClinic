c693e9fce2f7b34a31018725c60b34052232e932
ccunning2 <cameron.cunning@huskers.unl.edu>
2018-11-15 17:37:16 -0600

    Initial Project Commit

---
+++ src/test/java/com/cameroncunning/my_pet_clinic/MyPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cameroncunning.my_pet_clinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
