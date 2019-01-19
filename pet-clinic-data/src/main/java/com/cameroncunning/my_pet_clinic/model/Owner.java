fab43bcdcb53ab797cd0bc9f918a88d7d44978ca
ccunning2 <cameron.cunning@huskers.unl.edu>
2019-01-19 13:19:49 -0600

    Add properties to Owner, closes #28

--- pet-clinic-data/src/main/java/com/cameroncunning/my_pet_clinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/cameroncunning/my_pet_clinic/model/Owner.java
@@ -3,7 +3,34 @@ package com.cameroncunning.my_pet_clinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String ciry;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCiry() {
+        return ciry;
+    }
+
+    public void setCiry(String ciry) {
+        this.ciry = ciry;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
