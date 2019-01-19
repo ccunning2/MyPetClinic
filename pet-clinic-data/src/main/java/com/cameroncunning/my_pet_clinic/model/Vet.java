3554b38ac39d502f3cdbc9370b537f4646e1f036
ccunning2 <cameron.cunning@huskers.unl.edu>
2019-01-19 13:16:51 -0600

    Add speciaility, add to vet, closes #27

--- pet-clinic-data/src/main/java/com/cameroncunning/my_pet_clinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/cameroncunning/my_pet_clinic/model/Vet.java
@@ -1,4 +1,16 @@
 package com.cameroncunning.my_pet_clinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialities;
+
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
 }
